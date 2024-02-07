985b3872fb705fbe864022b0521a1a453f39b9db
tut211302 <153628638+tut211302@users.noreply.github.com>
2024-02-07 10:16:21 +0900

    Update InformationEstimator.java

--- s4/B211302/InformationEstimator.java
+++ s4/B211302/InformationEstimator.java
@@ -101,14 +101,14 @@ public class InformationEstimator implements InformationEstimatorInterface {
                 // System.out.print("("+start+","+end+")");
                 myFrequencer.setTarget(subBytes(myTarget, start, end));
 		int freq = myFrequencer.frequency();
-                value1 = value1 + f(myFrequencer.frequency());
+                
 		// it should  -->   value1 = value1 + f(myFrequencer.subByteFrequency(start, end)
 		// note that subByteFrequency is not work for B211302 version.
-		if(freq < 0){
+		if(freq == 0){
 			value1 = Double.MAX_VALUE;
 			break;
 		}
-		if(freq == 0) return (double) 0.0;
+		if(freq < 0) return (double) 0.0;
 		value1 = value1 + f(freq);
 		start = end;
 	    }
